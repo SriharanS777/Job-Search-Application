406389aa000d319656a6a90dc8c5ace5476ae89a
Asus <sriharans200802@gmail.com>
2024-11-17 02:15:03 -0800

    17.11.2024 - first commit

---
+++ app/src/main/java/com/example/jobfindapp/Splashscreen.java
@@ -0,0 +1,31 @@
+package com.example.jobfindapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+@SuppressLint("CustomSplashScreen")
+public class Splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent Intent = new Intent(Splashscreen.this, MainActivity.class);
+                Object startActivity;
+                startActivity(Intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
